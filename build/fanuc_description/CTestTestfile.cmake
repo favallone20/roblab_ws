5f8e565db63acce71e90b08e0ec2624f5dcb067e
favallone20 <f.avallone20@studenti.unisa.it>
2021-07-27 09:26:09 +0200

    Remove build directory

--- build/fanuc_description/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/francesco/Projects/roblab_ws/src/ex2_fanuc/fanuc_description
-# Build directory: /home/francesco/Projects/roblab_ws/build/fanuc_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/teleop_twist_keyboard_cpp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/francesco/Projects/roblab_ws/src/exercise0_husky/teleop_twist_keyboard_cpp
-# Build directory: /home/francesco/Projects/roblab_ws/build/teleop_twist_keyboard_cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
