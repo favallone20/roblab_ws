620c044a3f4ca14a1714a19e70a0ae68f0a83a8a
favallone20 <f.avallone20@studenti.unisa.it>
2021-08-26 08:41:39 +0200

    revision ex1

--- src/ex1/publisher_subscriber/src/publisher.cpp
+++ src/ex1/publisher_subscriber/src/publisher.cpp
@@ -3,32 +3,26 @@
 
 int main(int argc, char *argv[])
 {
-    ros::init(argc, argv, "publisher");
-    ros::NodeHandle nh; //The node handle is the access point for communications with the ROS system
-    ros::Publisher publisher = nh.advertise<sensor_msgs::JointState>("chatter", 1);
-    ros::Rate loopRate(10);
+  ros::init(argc, argv, "publisher");
+  ros::NodeHandle nh;  // The node handle is the access point for communications with the ROS system
+  ros::Publisher publisher = nh.advertise<sensor_msgs::JointState>("joint_states_ex1", 1);
+  ros::Rate loopRate(10);
 
-    while(ros::ok()){
-        sensor_msgs:: JointState jt;
-         jt.name.resize(6);   
-        for (int i = 0; i < 6; i++)
-        {
-             jt.name[i]="joint_"+ std::to_string(i);
-        }
-
-        for (int i = 0; i < 6; i++)
-        { 
-            jt.position.resize(5);
-            for (int j = 0; j < 5; j++)
-            {
-                jt.position[j] = rand();
-            }
-        }
-
-        publisher.publish(jt);
-        ros::spinOnce();
-        loopRate.sleep();
+  while (ros::ok())
+  {
+    sensor_msgs::JointState jt;
+    jt.name.resize(6);
+    jt.position.resize(6);
+    for (int i = 0; i < 6; i++)
+    {
+      jt.name[i] = "joint_" + std::to_string(i);
+      jt.position[i] = rand();
     }
-    ros::shutdown();
-    return 0;
+
+    publisher.publish(jt);
+    ros::spinOnce();
+    loopRate.sleep();
+  }
+  ros::shutdown();
+  return 0;
 }
--- src/ex1/publisher_subscriber/src/subscriber.cpp
+++ src/ex1/publisher_subscriber/src/subscriber.cpp
@@ -2,18 +2,17 @@
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
 
-
-void chatterCallback(const sensor_msgs::JointState jointState)
+void jointStatesCallback(const sensor_msgs::JointState jointState)
 {
-    ROS_INFO_STREAM("message received: "<<jointState);
+  ROS_INFO_STREAM("message received: " << jointState);
 }
 
 int main(int argc, char *argv[])
 {
-    ros::init(argc, argv, "listener");
-    ros::NodeHandle nh;
-    ros::Subscriber subscriber = nh.subscribe("chatter",10, chatterCallback);
-    ros::spin();
-    ros::shutdown();
-    return 0;
+  ros::init(argc, argv, "listener");
+  ros::NodeHandle nh;
+  ros::Subscriber subscriber = nh.subscribe("joint_states_ex1", 10, jointStatesCallback);
+  ros::spin();
+  ros::shutdown();
+  return 0;
 }
